a464b6fa6d40432b20dbcbce494a72e7e81e3d6a
Tchollo88 <Tcholloman@gmail.com>
2025-02-19 11:06:43 -0800

    Add project files.

---
+++ FoodTruckApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FoodTruckApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
