1b3d310f0d76ccc2a43799d5736206ea1d386919
Tchollo88 <Tcholloman@gmail.com>
2025-02-26 12:31:07 -0800

    Created New Customer Project  to set customer interaction seperate from the admin side

---
+++ FoodTruckCustomer/Models/Menu/Special.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodTruckCustomer.Models.Menu
+{
+    public class Special
+    {
+        [Key]
+        public int Special_ID { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public decimal Discount { get; set; } = decimal.MinValue;
+        [Required]
+        public DateOnly StartDate { get; set; }
+        [Required]
+        public DateOnly EndDate { get; set; }
+    }
+}
