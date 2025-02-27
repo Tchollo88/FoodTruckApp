2948705decd344bdd5802bff342931b8b8084797
christopher cane <christopher.cane@student.centralia.edu>
2025-02-26 19:14:46 -0800

    added customer and admin generic controllers

--- FoodTruckCustomer/Models/Menu/Receipt.cs
+++ FoodTruckCustomer/Models/Menu/Receipt.cs
@@ -1,7 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 using FoodTruckCustomer.Models.Client;
-using food
+
 
 
 namespace FoodTruckCustomer.Models.Menu
