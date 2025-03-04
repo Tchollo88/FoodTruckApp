4e131f2ad6d6d4fe76e0e9f2d190bd46d5f6639b
Tchollo88 <Tcholloman@gmail.com>
2025-03-04 10:41:36 -0800

    Added Customer Repo and interface

---
+++ Repository/Data/CustItemRepo.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Repository.Models.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data
+{
+    public class CustItemRepo : ICustItemRepo
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustItemRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Item>> GetAllItemsAsync()
+        {
+            return await _context.Items.ToListAsync();
+        }
+        public async Task<Item> GetItemByIdAsync(int id)
+        {
+            return await _context.Items.FindAsync(id);
+        }
+    }
+
+}
---
+++ Repository/Data/ICustItemRepo.cs
@@ -0,0 +1,15 @@
+﻿using Repository.Models.Menu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Data
+{
+    public interface ICustItemRepo
+    {
+        Task<IEnumerable<Item>> GetAllItemsAsync();
+        Task<Item> GetItemByIdAsync(int id);
+    }
+}
