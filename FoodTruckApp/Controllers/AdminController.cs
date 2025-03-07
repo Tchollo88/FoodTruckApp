a922ec8c90dc5a06ff63ba3d145f3b33c4cebac6
InventEverything <ian.engle@student.centralia.edu>
2025-03-07 10:11:54 -0800

    AdminController Items (list) and view.
    Items link appears in nav bar now and correctly navigates to a list of items. Edit, Details, and Delete buttons will automatically function once those actions are added to the admincontroller.

--- FoodTruckApp/Controllers/AdminController.cs
+++ FoodTruckApp/Controllers/AdminController.cs
@@ -1,14 +1,25 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Repository.Data;
 
 namespace FoodTruckApp.Controllers
 {
     [Authorize(Roles = "Admin")] // Restrict to Admins only
     public class AdminController : Controller
     {
+        private readonly IItemRepository _ItemRepository;
+        public AdminController(IItemRepository itemRepository)
+        {
+            _ItemRepository = itemRepository;
+        }
         public IActionResult Index()
         {
             return View(); // Looks for Views/Admin/Index.cshtml
+        }
+        public async Task<IActionResult> Items()
+        {
+            var items = await _ItemRepository.GetAllItemsAsync();
+            return View(items);
         }
     }
 }
