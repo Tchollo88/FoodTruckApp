7fbcc93998fdc276209e62ff109efc10bafec954
Vibe Rantz <subidriver03@gmail.com>
2025-03-06 21:55:32 -0800

    Admin Login / Admin Index Added

---
+++ FoodTruckApp/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodTruckApp.Controllers
+{
+    [Authorize(Roles = "Admin")] // Restrict to Admins only
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(); // Looks for Views/Admin/Index.cshtml
+        }
+    }
+}
