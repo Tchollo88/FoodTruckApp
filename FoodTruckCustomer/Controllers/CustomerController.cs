f643274ca8379307b939af3c0ae54b87de9087f3
Tchollo88 <Tcholloman@gmail.com>
2025-03-07 12:39:50 -0800

    Changed Customer controller name for syntext

---
+++ FoodTruckCustomer/Controllers/CustomerController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodTruckCustomer.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: CustomerController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: CustomerController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //GET: CustomerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        // POST: CustomerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
